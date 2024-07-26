43f89e51b3cb86a5ea1194a82ad8427f6194c6a5
Franck <136079691+Freddyede@users.noreply.github.com>
2024-07-26 16:46:29 +0200

    Update

--- tests/mock/InputConsoleMock.ts
+++ tests/mock/InputConsoleMock.ts
@@ -5,6 +5,7 @@ export let mockInput: jest.Mocked<InputConsole>;
 beforeEach(() => {
     mockInput  = new InputConsole() as jest.Mocked<InputConsole>;
 })
+// test
 afterAll(() => {
     mockInput.close();
 });
